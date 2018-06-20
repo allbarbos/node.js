9c4da1ece184bb9dc60ded74c2bead6e0f02a38d
Allan Barbosa <allan.barbosa@i-value.com.br>
2018-06-20 13:16:08 -0300

    endpoint de pagamento POST

--- exer01/routers/pagamentos.js
+++ exer01/routers/pagamentos.js
@@ -2,4 +2,19 @@ module.exports = function(app) {
     app.get('/', function(req, res) {
         res.send('Roda roda Jequiti!');
     });
+
+    app.get('/pagamentos', function(req, res) {
+        res.send('Você em pagamentos');
+    });
+
+    app.post('/pagamentos/pagamento', function(req, res) {
+        var pagamento = req.body;
+
+        pagamento.status = 'created';
+        pagamento.data = new Date;
+      
+        console.log(pagamento);
+
+        res.send(pagamento);
+    });
 }
