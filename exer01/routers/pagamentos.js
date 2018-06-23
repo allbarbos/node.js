981c93ee4d342c61b32e07f44e9757eb376d8406
Allan Barbosa <allan.hb@outlook.com>
2018-06-22 22:04:33 -0300

    Implementacão de erros HTTP e header location

--- exer01/routers/pagamentos.js
+++ exer01/routers/pagamentos.js
@@ -10,6 +10,18 @@ module.exports = function(app) {
     app.post('/pagamentos/pagamento', function(req, res) {
         var pagamento = req.body;
 
+        req.assert("forma_de_pagamento", "Forma de pagamento é obrigatória.").notEmpty();
+        req.assert("valor", "Valor é obrigatório e deve ser um decimal.").notEmpty().isFloat();
+        req.assert("moeda", "Moeda é obrigatória e deve ter 3 caracteres").notEmpty().len(3,3);
+
+        var errors = req.validationErrors();
+
+        if (errors){
+            console.log("Erros de validação encontrados");
+            res.status(400).send(errors);
+            return;
+        }
+
         pagamento.status = 'created';
         pagamento.data = new Date;
 
@@ -19,10 +31,15 @@ module.exports = function(app) {
         pagamentoDao.salva(pagamento, function(exception, result){
             if (exception) {
                 console.log('##### ERRO ##### ' + exception);
-                res.status(400).send(exception);
+                res.status(500).send(exception);
             } else {
                 console.log('pagamento criado: ' + result);
-                res.json(pagamento);
+                
+                pagamento.id = result.insertId;
+
+                res.status(201)
+                   .location(`/pagamentos/pagamento/${result.insertId}`)
+                   .json(pagamento);
             }
         });
     });
