f1293f777d8315da71803ea5da90b5a58b3ff369
Allan Barbosa <allan.hb@outlook.com>
2018-06-26 23:52:05 -0300

    Configuração do consign para carregar rotas

--- udy-exer01/config/server.js
+++ udy-exer01/config/server.js
@@ -1,6 +1,9 @@
 const app = require('express')();
+const consign = require('consign')();
 
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
+
+consign.include('app/routes').into(app);
 
 module.exports = app;
