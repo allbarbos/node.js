3b8713aaebc997f9011102f0cc1e050320676889
Allan Barbosa <allan.barbosa@i-value.com.br>
2018-07-30 14:53:10 -0300

    Implementação de Mutation

--- graphql/src/app.ts
+++ graphql/src/app.ts
@@ -12,9 +12,10 @@ class App {
     }
 
     private middleware(): void {
-
+        
         this.express.use('/graphql', graphqlHTTP({
-            schema: schema
+            schema: schema,
+            graphiql: process.env.NODE_ENV.trim() === 'development'
         }));
     }
 }
