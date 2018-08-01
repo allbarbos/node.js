d3a13e61427f424abdacd1827051d059b895f2bf
Allan Barbosa <allan.barbosa@i-value.com.br>
2018-08-01 09:22:26 -0300

    Implementação PostModel

--- graphql/src/interfaces/ModelsInterface.ts
+++ graphql/src/interfaces/ModelsInterface.ts
@@ -1,3 +1,7 @@
+import { PostModel } from "../models/PostModel";
+import { UserModel } from "../models/UserModel";
+
 export interface ModelsInterface {
-    
+    Post: PostModel;
+    User: UserModel;
 }
