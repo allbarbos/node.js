6d0d52c95e0a34e0051f1124ca0deb8342e7fb55
Allan Barbosa <allan.hb@outlook.com>
2018-09-23 15:45:01 -0300

    config ejs

--- completo/src/app.js
+++ completo/src/app.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
+app.get('/tecnologia', function (req, res) {
+  res.render('secao/tecnologia')
+})
+
+app.get('', function (req, res) {
+  res.send('Home page!')
+})
+
 app.listen(3000, function() {
   console.log("Rodando na porta 3000");
 });
