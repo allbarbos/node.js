0cef91559ce3d0d83d4b457c21654dd5dbc38a0c
Allan Barbosa <allan.barbosa@ingaia.com.br>
2018-09-27 11:29:49 -0300

    configuração de rotas com consign

--- completo/src/app.js
+++ completo/src/app.js
@@ -1,7 +1,4 @@
 const app = require('./config/server');
-require('./app/routes/home')(app);
-require('./app/routes/noticias')(app);
-require('./app/routes/formulario_inclusao_noticia')(app);
 
 app.listen(3000, function() {
   console.log("Rodando na porta 3000");
--- completo/src/config/server.js
+++ completo/src/config/server.js
@@ -1,7 +1,10 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const consign = require('consign');
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', './app/views')
+
+consign().include('app/routes').into(app);
 
 module.exports = app;
