615aa3edec5330f6f4f183a0d407b9295c980b4d
Allan Barbosa <allan.barbosa@ingaia.com.br>
2018-10-03 11:37:28 -0300

    Refatoração views

--- completo/src/app/models/NoticiaDAO.js
+++ completo/src/app/models/NoticiaDAO.js
@@ -2,12 +2,12 @@ function NoticiaDAO(cnx){
     this._cnx = cnx;
 }
 
-NoticiaDAO.prototype.getNoticia = function(callback) {
-    this._cnx.query('select * from noticias where id_noticia = 2', callback);
+NoticiaDAO.prototype.getNoticias = function(callback) {
+    this._cnx.query('select * from noticias order by data_criacao desc', callback);
 };
 
-NoticiaDAO.prototype.getNoticias = function(callback) {
-    this._cnx.query('select * from noticias', callback);
+NoticiaDAO.prototype.getNoticia = function(callback) {
+    this._cnx.query('select * from noticias where id_noticia = 2', callback);
 };
 
 NoticiaDAO.prototype.getNewsByLimit = function(limit, callback) {    
