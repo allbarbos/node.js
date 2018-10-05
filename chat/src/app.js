262f5436c6cc7b50dc58c0c7cb551890cac5c7f0
Allan Barbosa <allan.barbosa@ingaia.com.br>
2018-10-05 17:26:20 -0300

    configuração do socket.io

--- chat/src/app.js
+++ chat/src/app.js
@@ -1,5 +1,14 @@
 const app = require('./config/server');
 
-app.listen(3000, function() {
+const server = app.listen(3000, function() {
   console.log("Rodando na porta 3000");
+});
+
+const io = require('socket.io').listen(server);
+io.on('connection', function(socket){
+  console.log('connection');
+
+  socket.on('disconnect', function(){
+    console.log('disconnect');
+  })
 });
