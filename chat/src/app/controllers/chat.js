8c15758d4d9ca8038c6006c8040ce733bb3100ac
Allan Barbosa <allan.barbosa@ingaia.com.br>
2018-10-05 17:16:27 -0300

    enviar form

--- chat/src/app/controllers/chat.js
+++ chat/src/app/controllers/chat.js
@@ -1,3 +1,15 @@
 module.exports.iniciaChat = function(application, req, res) {
+    const dados = req.body;
+    console.log(dados)
+    
+    req.assert('apelido', 'Nome/apelido obrigatório').notEmpty();
+    req.assert('apelido', 'Nome/apelido deve conter entre 3 e 15 caracteres').len(3, 15);
+    
+    const erros = req.validationErrors();
+    if(erros) {
+        res.render('index', { erros })
+        return;
+    }
+
     res.render("chat");
 }
