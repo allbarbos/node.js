33c9039f5cec71c7d198e27509e25da3f2e799f5
Allan Barbosa <allan.hb@outlook.com>
2019-02-20 16:22:29 -0300

    docker

--- ocr/src/services/aws.js
+++ ocr/src/services/aws.js
@@ -1,5 +1,5 @@
 const AWS = require('aws-sdk');
-exports.AWS.config.update({
+AWS.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
