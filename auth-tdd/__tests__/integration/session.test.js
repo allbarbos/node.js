36ed720bb29eac55bddade729c9be1a424a4968c
Allan Barbosa <allan.hb@outlook.com>
2019-02-26 15:48:57 -0300

    fim da configuração dos testes

--- auth-tdd/__tests__/integration/session.test.js
+++ auth-tdd/__tests__/integration/session.test.js
@@ -1,11 +1,26 @@
-describe('Authentication', () =>{
-    it('should sum two numbers', () =>{
-        const x = 2;
-        const y = 4;
-        const sum = x + y;
-        expect(sum).toBe(7)
-    });
-    // it('should receive JWT token when authenticated with valid credentials', () =>{
+const request = require('supertest');
 
-    // });
+const app = require('../../src/app');
+const { User } = require('../../src/app/models');
+const truncate = require('../utils/truncate');
+
+describe('Authentication', () => {
+    beforeEach( async () => await truncate() )
+
+    it('should authenticate with valid credentials', async () =>{
+        const user = await User.create({
+            name: 'Teste',
+            email: 'teste@teste.com',
+            password_hash: '1234'
+        })
+
+        const response = await request(app)
+            .post('/sessions')
+            .send({
+                email: user.email,
+                password: "123456"
+            });
+        
+        expect(response.status).toBe(200);
+    });    
 });
