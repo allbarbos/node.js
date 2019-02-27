87915e41c4a794f2af73885fd179831fcb263f0c
Allan Barbosa <allan.hb@outlook.com>
2019-02-27 16:02:11 -0300

    unit test - user password encrypt

---
+++ auth-tdd/__tests__/unit/user.test.js
@@ -0,0 +1,19 @@
+const bcrypt = require('bcryptjs');
+
+const { User } = require('../../src/app/models');
+const truncate = require('../utils/truncate');
+
+describe('User', () => { 
+    beforeEach( async () => await truncate() )
+
+    it('should encrypt user password', async () => {
+        const user = await User.create({
+            name: 'Allan',
+            email: 'allan@teste.com',
+            password: '1234'
+        });
+
+        const compareHash = await bcrypt.compare('1234', user.password_hash);
+        expect(compareHash).toBe(true);
+    });
+})
